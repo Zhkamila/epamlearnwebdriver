2af1562b596ca0a357a4107f3fcf5e6f38cb7526
Zhkamila <42671770+Zhkamila@users.noreply.github.com>
2021-03-28 21:48:08 +0600

    Update WebDriverStart.java

--- src/test/java/WebDriverStart.java
+++ src/test/java/WebDriverStart.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-reformimport org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
