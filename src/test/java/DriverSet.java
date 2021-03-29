58ffb6d3b6958cd964480bfba1628bdd6ab5827a
Zhkamila <kamila.zhanuzak@nu.edu.kz>
2021-03-29 12:53:55 +0600

    added getDriver()

---
+++ src/test/java/DriverSet.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSet {
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+}
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,12 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WebDriverStart {
-
+public class Runner {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = DriverSet.getDriver();
         driver.get("https://translate.google.com/?sl=en&tl=ru&op=translate");
 
         WebElement textArea = driver.findElement(By.xpath("//*[@aria-label='Source text']"));
